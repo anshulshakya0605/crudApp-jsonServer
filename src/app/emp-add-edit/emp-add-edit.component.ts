94a1c47624bac25ce4977d24782264b2c7f19934
anshulshakya0605 <anshulshakya0605@gmail.com>
2024-01-12 22:42:01 +0530

    fix email error

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -48,7 +48,7 @@ email = new FormControl('', [Validators.required, Validators.email])
     this.empForm = this.fb.group({
       firstName: '',
       lastName: '',
-      email: '',
+      email: ['',Validators.email],
       password: '',
       gender: '',
       education: '',
@@ -88,13 +88,6 @@ email = new FormControl('', [Validators.required, Validators.email])
       }
     };
   };
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
   cancelForm(){
     this.dialogRef.close();
   }
